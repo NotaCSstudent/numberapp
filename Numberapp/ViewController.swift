083b30558fc82cd8fe3a490076ecd935c2550dd4
Mahamud Babu <mahmudbabu94@gmail.com>
2020-02-26 21:08:19 -0500

    Number app

--- Numberapp/ViewController.swift
+++ Numberapp/ViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func randomNumber(_ sender: Any) {
+        
+        let randomNumber = Int.random(in: 100...999)
+        label.text = String(randomNumber)
+        
+    }
+    
 }
 
